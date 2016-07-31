a389ea7d51bd4691dfcfca9806661808629a6f5d
nina07 <niyanta150792@gmail.com>
2016-07-31 16:56:27 +0530

    First commit of facechat

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,34 @@
+class UsersController < ApplicationController
+  def index
+    byebug
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.save
+    redirect_to @user
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
+end
